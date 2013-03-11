0e8f193a426d4651319cec4da7e4231cf060c836
root <root@dkineev-EasyNote-TX86.(none)>
2013-03-11 18:50:26 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site::Application.config.secret_token = 'ff070c523a4968776acbf88ecb6fd473fd40e33e5ca5a8f2a9ed1848d9920573ebc581462c41a55612cd2f2b29782640bfaab65bc4fac7dd631afccd1cf4b843'
